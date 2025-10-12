766929da4541cef9a270dbcd34c66c5faf6e8e28
maks2134 <maks210306@yandex.by>
2025-10-12 14:52:37 +0300

    lab2 + lab3

---
+++ lab3/internal/packet/cycliccode.go
@@ -0,0 +1,145 @@
+package packet
+
+import (
+	"math/rand"
+	"time"
+)
+
+type CyclicCode struct {
+	generator uint16
+	fcsLength int
+}
+
+func NewCyclicCode() *CyclicCode {
+	return &CyclicCode{
+		generator: 0x107,
+		fcsLength: 8,
+	}
+}
+
+func (cc *CyclicCode) CalculateFCS(data string) uint8 {
+	if len(data) == 0 {
+		return 0
+	}
+
+	dataBinary := BytesToBinaryString(data)
+	remainder := uint64(0)
+
+	for i := 0; i < len(dataBinary); i++ {
+		remainder = (remainder << 1) | uint64(dataBinary[i]-'0')
+
+		if remainder >= (1 << 9) {
+			remainder ^= uint64(cc.generator)
+		}
+	}
+
+	return uint8(remainder)
+}
+
+func (cc *CyclicCode) VerifyFCS(data string, receivedFCS uint8) bool {
+	calculatedFCS := cc.CalculateFCS(data)
+	return calculatedFCS == receivedFCS
+}
+
+func (cc *CyclicCode) polynomialDivision(dividend uint64, dividendLength int) uint64 {
+	generatorAligned := uint64(cc.generator) << (dividendLength - 9)
+	remainder := dividend
+
+	for i := 0; i < dividendLength-8; i++ {
+		if remainder&(1<<(dividendLength-1-i)) != 0 {
+			remainder ^= generatorAligned >> i
+		}
+	}
+
+	return remainder & 0xFF
+}
+
+func (cc *CyclicCode) DetectErrors(data string, receivedFCS uint8) (bool, int, string) {
+	calculatedFCS := cc.CalculateFCS(data)
+
+	if calculatedFCS == receivedFCS {
+		return false, 0, data
+	}
+
+	syndrome := calculatedFCS ^ receivedFCS
+
+	correctedData, correctionSuccess := cc.correctSingleError(data, syndrome)
+	if correctionSuccess {
+		return true, 1, correctedData
+	}
+
+	return true, 2, data
+}
+
+func (cc *CyclicCode) correctSingleError(data string, syndrome uint8) (string, bool) {
+	dataBinary := BytesToBinaryString(data)
+
+	for i := 0; i < len(dataBinary); i++ {
+		flippedData := []byte(dataBinary)
+		if flippedData[i] == '0' {
+			flippedData[i] = '1'
+		} else {
+			flippedData[i] = '0'
+		}
+
+		correctedDataStr := BinaryStringToBytes(string(flippedData))
+		correctedFCS := cc.CalculateFCS(correctedDataStr)
+
+		if correctedFCS == syndrome {
+			return correctedDataStr, true
+		}
+	}
+
+	return data, false
+}
+
+func (cc *CyclicCode) SimulateBitCorruption(data string) string {
+	rand.Seed(time.Now().UnixNano())
+
+	errorType := rand.Float32()
+	dataBinary := []byte(BytesToBinaryString(data))
+
+	if errorType < 0.25 {
+		bitPos := rand.Intn(len(dataBinary))
+		if dataBinary[bitPos] == '0' {
+			dataBinary[bitPos] = '1'
+		} else {
+			dataBinary[bitPos] = '0'
+		}
+	} else {
+		bitPos1 := rand.Intn(len(dataBinary))
+		bitPos2 := rand.Intn(len(dataBinary))
+
+		for bitPos2 == bitPos1 {
+			bitPos2 = rand.Intn(len(dataBinary))
+		}
+
+		if dataBinary[bitPos1] == '0' {
+			dataBinary[bitPos1] = '1'
+		} else {
+			dataBinary[bitPos1] = '0'
+		}
+
+		if dataBinary[bitPos2] == '0' {
+			dataBinary[bitPos2] = '1'
+		} else {
+			dataBinary[bitPos2] = '0'
+		}
+	}
+
+	return BinaryStringToBytes(string(dataBinary))
+}
+
+func (cc *CyclicCode) GetFCSLength() int {
+	return cc.fcsLength
+}
+
+func binaryStringToUint(binaryStr string) uint64 {
+	var result uint64
+	for i, bit := range binaryStr {
+		if bit == '1' {
+			result |= 1 << (len(binaryStr) - 1 - i)
+		}
+	}
+	return result
+}
---
+++ lab3/internal/serialterminal/serial_terminal.go
@@ -0,0 +1,275 @@
+package serialterminal
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"oks/internal/packet"
+
+	"fyne.io/fyne/v2"
+	"github.com/tarm/serial"
+)
+
+type SerialTerminal struct {
+	port        io.ReadWriteCloser
+	portName    string
+	dataBits    int
+	stopReading chan bool
+	messageChan chan string
+	packetChan  chan string
+	bitStuffer  *packet.BitStuffer
+	OnMessage   func(string)
+	OnStatus    func(string)
+	OnPacket    func(string)
+}
+
+func New(name string) *SerialTerminal {
+	return &SerialTerminal{
+		portName:    name,
+		dataBits:    8,
+		stopReading: make(chan bool, 1),
+		messageChan: make(chan string, 100),
+		packetChan:  make(chan string, 50),
+		bitStuffer:  packet.NewBitStuffer(),
+		OnMessage:   func(string) {},
+		OnStatus:    func(string) {},
+		OnPacket:    func(string) {},
+	}
+}
+
+func (st *SerialTerminal) SetPortName(name string) {
+	st.portName = name
+}
+
+func (st *SerialTerminal) SetDataBits(dataBits int) {
+	oldDataBits := st.dataBits
+	st.dataBits = dataBits
+
+	if st.port != nil && oldDataBits != dataBits {
+		log.Printf("Data bits changed from %d to %d, reconnecting...", oldDataBits, dataBits)
+		err := st.Disconnect()
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Millisecond * 100)
+		err = st.Connect()
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (st *SerialTerminal) GetDataBits() int {
+	return st.dataBits
+}
+
+func (st *SerialTerminal) GetPortName() string {
+	return st.portName
+}
+
+func (st *SerialTerminal) IsConnected() bool {
+	return st.port != nil
+}
+
+func (st *SerialTerminal) Connect() error {
+	c := &serial.Config{
+		Name:        st.portName,
+		Baud:        9600,
+		ReadTimeout: time.Second * 1,
+		Size:        byte(st.dataBits),
+		Parity:      serial.ParityNone,
+		StopBits:    serial.Stop1,
+	}
+
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		return st.formatError("open", err)
+	}
+
+	st.port = s
+	if st.OnStatus != nil {
+		st.OnStatus(fmt.Sprintf("Port %s open", st.portName))
+	}
+	log.Printf("Port %s opened successfully", st.portName)
+
+	go st.readPort()
+	go st.messageHandler()
+
+	return nil
+}
+
+func (st *SerialTerminal) Disconnect() error {
+	if st.port != nil {
+		select {
+		case st.stopReading <- true:
+		default:
+		}
+
+		err := st.port.Close()
+		if err != nil {
+			return st.formatError("close", err)
+		}
+
+		st.port = nil
+		if st.OnStatus != nil {
+			st.OnStatus("Port closed")
+		}
+		log.Printf("Port %s closed", st.portName)
+	}
+	return nil
+}
+
+func (st *SerialTerminal) formatError(operation string, err error) error {
+	if os.IsNotExist(err) {
+		return fmt.Errorf("serial port %s does not exist", st.portName)
+	}
+	if os.IsPermission(err) {
+		return fmt.Errorf("permission denied for port %s", st.portName)
+	}
+	return fmt.Errorf("failed to %s port %s: %v", operation, st.portName, err)
+}
+
+func (st *SerialTerminal) applyDataBitMask(msg string) string {
+	if st.dataBits >= 8 {
+		return msg
+	}
+
+	mask := byte((1 << st.dataBits) - 1)
+	result := make([]byte, len(msg))
+	for i, char := range []byte(msg) {
+		result[i] = char & mask
+	}
+
+	return string(result)
+}
+
+func (st *SerialTerminal) SendPacket(address, control byte, data string) error {
+	if st.port == nil {
+		return fmt.Errorf("port is not open")
+	}
+
+	p := packet.NewPacket(address, control, data)
+
+	p.SimulateCorruption()
+	log.Printf("Bit corruption simulated for packet: Address=0x%02X, Control=0x%02X, Data=%s, FCS=0x%02X",
+		address, control, p.Data, p.FCS)
+
+	stuffedData := st.bitStuffer.StuffPacket(p)
+
+	packetInfo := st.bitStuffer.GetStuffedFrameInfo(p)
+	st.packetChan <- packetInfo
+
+	_, err := st.port.Write([]byte(stuffedData))
+	if err != nil {
+		return st.formatError("write to", err)
+	}
+
+	log.Printf("Packet sent to %s: Address=0x%02X, Control=0x%02X, Data=%s, FCS=0x%02X",
+		st.portName, address, control, p.Data, p.FCS)
+	st.messageChan <- "TX:" + p.Data
+
+	return nil
+}
+
+func (st *SerialTerminal) SendMessage(msg string) error {
+	address := byte(0x01)
+	if strings.Contains(st.portName, "ttys003") {
+		address = 0x02
+	}
+
+	return st.SendPacket(address, 0x00, msg)
+}
+
+func (st *SerialTerminal) messageHandler() {
+	for {
+		select {
+		case msg := <-st.messageChan:
+			if st.OnMessage != nil {
+				fyne.Do(func() { st.OnMessage(msg) })
+			}
+		case packetInfo := <-st.packetChan:
+			if st.OnPacket != nil {
+				fyne.Do(func() { st.OnPacket(packetInfo) })
+			}
+		case <-st.stopReading:
+			return
+		}
+	}
+}
+
+func (st *SerialTerminal) readPort() {
+	buf := make([]byte, 512)
+	var receivedData string
+
+	for {
+		select {
+		case <-st.stopReading:
+			log.Printf("Reading stopped for port %s", st.portName)
+			return
+		default:
+			if st.port == nil {
+				return
+			}
+
+			n, err := st.port.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					time.Sleep(time.Millisecond * 100)
+					continue
+				}
+				log.Printf("Error reading from port %s: %v", st.portName, err)
+				time.Sleep(time.Second * 1)
+				continue
+			}
+
+			if n > 0 {
+				receivedData += string(buf[:n])
+
+				flag := byte(0x0E)
+				for {
+					startIdx := strings.IndexByte(receivedData, flag)
+					if startIdx == -1 {
+						if len(receivedData) > 1024 {
+							receivedData = ""
+						}
+						break
+					}
+
+					endIdx := strings.IndexByte(receivedData[startIdx+1:], flag)
+					if endIdx == -1 {
+						break
+					}
+					endIdx += startIdx + 1
+
+					frameData := receivedData[startIdx : endIdx+1]
+					packetObj := st.bitStuffer.DestuffPacket(frameData)
+					receivedData = receivedData[endIdx+1:]
+
+					if packetObj != nil {
+						hasErrors, errorCount, correctedData := packetObj.DetectAndCorrectErrors()
+
+						if !hasErrors {
+							log.Printf("Packet received from %s: Address=0x%02X, Control=0x%02X, Data=%s, FCS=0x%02X",
+								st.portName, packetObj.Address, packetObj.Control, packetObj.Data, packetObj.FCS)
+							st.messageChan <- "RX:" + packetObj.Data
+						} else if errorCount == 1 {
+							log.Printf("Single error detected and corrected from %s: Original=%s, Corrected=%s, FCS=0x%02X",
+								st.portName, packetObj.Data, correctedData, packetObj.FCS)
+							st.messageChan <- "RX:" + correctedData + " (corrected)"
+						} else {
+							log.Printf("Double error detected from %s: Data=%s, FCS=0x%02X (cannot correct)",
+								st.portName, packetObj.Data, packetObj.FCS)
+							st.messageChan <- "RX: ERROR - Double bit error detected (cannot correct)"
+						}
+					}
+				}
+			}
+
+			time.Sleep(time.Millisecond * 10)
+		}
+	}
+}
